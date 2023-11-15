603e9fd8e861bf486bb19d2883af566c63f966c9
CodeSandbox <support@codesandbox.io>
2023-11-15 10:07:42 +0000

    Sandbox snapshot commit

--- widgets/carousel/component.tsx
+++ widgets/carousel/component.tsx
@@ -1,9 +1,18 @@
+import { cn } from "@/lib/utils";
 import { FC } from "react";
 
 interface Props {}
 const App: FC<Props> = () => {
-  return <div className={cn("flex items-center justify-center w-screen h-screen ")}>
-    <div className></div>
-  </div>
+  return (
+    <div className={cn("flex items-center justify-center w-screen h-screen ")}>
+      <div className={cn(`w-[60%] h-[50%] bg-pink-200`)}>
+        <div className="relative">
+          <div className=" w-[100px] h-[100px] bg-orange-200"></div>
+          <div className="absolutebso w-[150px] h-[150px] bg-slate-200"></div>
+          <div className="absolute w-[200px] h-[200px] bg-blue-300"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default App;
